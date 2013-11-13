415a6ddae3b4a39a9483b140034508e1fdc051bb
Astarcorp <corepbl@hotmail.com>
2013-11-13 13:18:41 +0800

    l

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   // Server
   require 'server/fb-php-sdk/facebook.php';
 
-  // Production
+  // Linking to Facebook Application
   $app_id = '418284238273760';
   $app_secret = '44926e66c8b6d95019865554ec58c9a2';
   $app_namespace = 'tradingwithfriends';
@@ -12,22 +12,22 @@
   $scope = 'email,publish_actions';
 
   // Init the Facebook SDK
-   $facebook = new Facebook(array(
+  $facebook = new Facebook(array(
      'appId'  => $app_id,
      'secret' => $app_secret,
    ));
 
    // Get the current user
-   $user = $facebook->getUser();
+  $user = $facebook->getUser();
 
    // If the user has not installed the app, redirect them to the Login Dialog
-   if (!$user) {
-     $loginUrl = $facebook->getLoginUrl(array(
-       'scope' => $scope,
-       'redirect_uri' => $app_url,
-     ));
-     print('<script> top.location.href=\'' . $loginUrl . '\'</script>');
-   }
+  if (!$user) {
+    $loginUrl = $facebook->getLoginUrl(array(
+      'scope' => $scope,
+      'redirect_uri' => $app_url,
+    ));
+    print('<script> top.location.href=\'' . $loginUrl . '\'</script>');
+  }
    
 ?>
 
@@ -36,11 +36,10 @@
   <head>
     <title>Trading with Friends!</title>
     
-    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Display Date and Time in Javascript</title>
-        <script type="text/javascript" src="scripts/date_time.js"></script>
+   	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <script type="text/javascript" src="scripts/date_time.js"></script>
+
       <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
@@ -52,34 +51,30 @@
       <script src="scripts/jquery.jCounter-0.1.4.js"></script>
       <!--[if IE]><script src="scripts/excanvas.js"></script><![endif]-->
       
-    <script>
-	</script>
-      
   </head>
-
   <body>
   
-  		<span id="date_time"></span>
-        <script type="text/javascript">window.onload = date_time('date_time');</script>
+	  <span id="date_time"></span>
+	  <script type="text/javascript">window.onload = date_time('date_time');</script>
       
       <div id="fb-root"></div>
       <script src="//connect.facebook.net/en_US/all.js"></script>
       
-      <p>A Star Crop</p>
+      <p>AStar Corp</p>
       <button onclick="myFunction()">Click Here!</button>
-      <p id="demo"></p>
+      <p id="welcome"></p>
       
       <script>
       function myFunction()
       {
       var x;
 
-      var person=prompt("Please enter your name","Harry Potter");
+      var person=prompt("Please enter your name","");
 
       if (person!=null)
         {
-        x="Hello " + person + "! Welcome to Trading With Friend";
-        document.getElementById("demo").innerHTML=x;
+        x="Hello " + person + "! Welcome to Trading with Friends";
+        document.getElementById("welcome").innerHTML=x;
         }
       }
       </script>
