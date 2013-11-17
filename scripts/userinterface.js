5421941d782bd616ce596fea6acc8de266a9240b
Astarcorp <corepbl@hotmail.com>
2013-11-17 13:09:23 +0800

    s

--- scripts/userinterface.js
+++ scripts/userinterface.js
@@ -15,11 +15,11 @@ setInterval(refresh, 1*1000); //refresh every 1 second
 
 $(document).ready(refresh_handler);
 
-$('div#menuDemo > ul > li').hover(function() {
+$("#menuDemo > ul > li").hover(function() {
 	//effect when the user hovers over the menu
 	//first hide the menu item, since the CSS displays it - then slide it down.
-   	$(this).children('ul').hide().slideDown();
+   	$(this).children("ul").hide().slideDown();
 }, function() {
 	//effect when the user leaves the current menu area - fade out
-	$(this).children('ul').fadeOut();
+	$(this).children("ul").fadeOut();
 });
