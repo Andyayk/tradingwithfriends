68f40696d5f85adedf900f6af1322cab348d49b9
Astarcorp <corepbl@hotmail.com>
2013-11-17 14:29:16 +0800

    try alot times

--- scripts/userinterface.js
+++ scripts/userinterface.js
@@ -12,33 +12,22 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$("#RecommendButton").click(function(){
-		
+			  var challengeData = {"challenge_score" : gScore};
+
+			  if (gScore) {
+			    FB.ui({method: 'apprequests',
+			      title: 'Friend Smash Challenge!',
+			      message: 'I just smashed ' + gScore + ' friends! Can you beat it?',
+			      data: challengeData
+			    }, fbCallback);
+			  }
+			  else {
+			    FB.ui({method: 'apprequests',
+			      title: 'Play Friend Smash with me!',
+			      message: 'Andyayk Rocks, come check that out!!!',
+			    }, fbCallback);
+			  }
+			}
 	});
 });
 
-var tradeButton = document.createElement('div');
-tradeButton.className = 'menu_item';
-tradeButton.id = 'trade_button';
-tradeButton.style.top = "188px";
-tradeButton.style.left = "0px";
-tradeButton.style.zIndex = "10";
-tradeButton.setAttribute('onclick', 'javascript:startGame()');
-tradeButton.style.backgroundImage = "url('images/button_trade.png')";
-menuContainer.appendChild(tradeButton);
-
-//Highlight the Trade Button when Hovered
-var tradeButtonHover = document.createElement('div');
-tradeButtonHover.className = 'menu_item';
-tradeButtonHover.style.top = "188px";
-tradeButtonHover.style.left = "0px";
-tradeButtonHover.style.backgroundImage = "url('images/button_trade_hot.png')";
-menuContainer.appendChild(tradeButtonHover);
-
-$("#trade_button").hover (
-  function() {
-    $(this).stop().animate({"opacity": "0"}, "slow");
-  },
-  function() {
-    $(this).stop().animate({"opacity": "1"}, "slow");
-  }
-);
