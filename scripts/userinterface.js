ab691e5bccb173d4c86ba1fa3f0962343251ced9
Astarcorp <corepbl@hotmail.com>
2013-11-17 14:16:25 +0800

    try

--- scripts/userinterface.js
+++ scripts/userinterface.js
@@ -10,3 +10,35 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$("#RecommendButton").click(function(){
+		
+	});
+});
+
+var tradeButton = document.createElement('div');
+tradeButton.className = 'menu_item';
+tradeButton.id = 'trade_button';
+tradeButton.style.top = "188px";
+tradeButton.style.left = "0px";
+tradeButton.style.zIndex = "10";
+tradeButton.setAttribute('onclick', 'javascript:startGame()');
+tradeButton.style.backgroundImage = "url('images/button_trade.png')";
+menuContainer.appendChild(tradeButton);
+
+//Highlight the Trade Button when Hovered
+var tradeButtonHover = document.createElement('div');
+tradeButtonHover.className = 'menu_item';
+tradeButtonHover.style.top = "188px";
+tradeButtonHover.style.left = "0px";
+tradeButtonHover.style.backgroundImage = "url('images/button_trade_hot.png')";
+menuContainer.appendChild(tradeButtonHover);
+
+$("#trade_button").hover (
+  function() {
+    $(this).stop().animate({"opacity": "0"}, "slow");
+  },
+  function() {
+    $(this).stop().animate({"opacity": "1"}, "slow");
+  }
+);
