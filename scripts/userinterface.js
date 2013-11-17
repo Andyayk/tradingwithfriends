b5f637def98ef6bb69ee08622a4f23fa307b7de0
Astarcorp <corepbl@hotmail.com>
2013-11-17 13:50:13 +0800

    f

--- scripts/userinterface.js
+++ scripts/userinterface.js
@@ -4,17 +4,6 @@ $(document).ready(function(){
 	});
 });
 
-function refresh_handler() {
-		 function refresh() {
-		$.get("index.php", null, function(data, textStatus) {
-  $("showEquity").html(data);
-});
-}
-setInterval(refresh, 1*1000); //refresh every 1 second
-}
-
-$(document).ready(refresh_handler);
-
 $("#menuDemo > ul > li").hover(function() {
 	//effect when the user hovers over the menu
 	//first hide the menu item, since the CSS displays it - then slide it down.
