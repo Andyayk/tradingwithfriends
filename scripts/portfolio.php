4329e8138e31aad46160f727fc73c810dc108ac8
junwei <corepbl@hotmail.com>
2013-11-18 23:57:07 +0800

    test1

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -23,63 +23,6 @@ Welcome to My Portfolio
    	 Quantity : <?php echo $_POST["quantity"]; ?><br>
 
 	</tr>
-   
-   <? foreach ($model['stocks'] as $stock): ?>
-   <tr>
-     <td><?= $stock->TICKER ?></td>
-     <td id="stock-price-<?= $stock->ID ?>"></td>
-    
-    <td id="stock-shares-<?= $stock->ID ?>">
-       <?= $stock->SHARES ?>
-     </td>
-
-    <!-- because we cannot get the innerHTML of the shares element in FBJS -->
-    <? if ($stock->SHARES): ?>
-      <script> 
-          portfolioShares[<?= $stock->ID ?>] = <?= $stock->SHARES ?>; 
-      </script>
-    <? endif; ?>
- 
-     <td id="stock-total-<?= $stock->ID ?>"></td>
-     <td class="row-controls">
-       <div>
-
-         <!-- buy/sell form for this stock -->
-         <form action="<?= $model['appUrl'] ?>/tradeStock">
-           <input type="submit" value="Buy/Sell" class="trade-button"/>
-           <input type="hidden" name="stockId" value="<?= $stock->ID ?>"/>
-           <input type="text" name="shares" class="shares"/>           
-           shares.
-         </form>
-
-         <!-- recommend this stock to your friends -->
-         <a href="<?= $model['appUrl'] ?>/recommendStockToFriends?ticker=<?= $stock->TICKER ?>"
-            class="recommend-control">
-           Recommend to your friends!
-         </a>
-
-       </div>
-
-       <? if ($model['tradeResult'] && $model['tradeResultStockId'] == $stock->ID): ?>       				       
-         Change: <span id="trade-result"></span>         
-
-	 <!-- leave the formatting to the javascript -->
-         <script>            
-	    tradeResult = <?= $model['tradeResult'] ?>;
-         </script>
-
-       <? endif; ?>
-
-     </td>
-   </tr>
-   <? endforeach; ?>
-   <tr class="footer">
-     <td>Total:</td><br>
-     <td id="total"></td>
-     <td></td>
-     <td></td>
-   </tr>
-</table>
 
 <div id="console">
     
