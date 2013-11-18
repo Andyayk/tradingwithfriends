6ed251c0974af1c008d6fa4577efae9c4cb81690
junwei <corepbl@hotmail.com>
2013-11-19 00:49:17 +0800

    test12

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -18,13 +18,45 @@ Welcome to My Portfolio
 <table>
    <tr>
    	 <form name="input" action="index.php" method="POST">
-   	
-   	 My Equity : <?php echo $_POST["name"]; ?><br>
-   	 Quantity : <?php echo $_POST["quantity"]; ?><br>
+   	 My Equity <?php echo $_POST["name"]; ?><br>
+	 Your Quantity : <?php echo $_POST["quantity"]; ?><br>
+	 
+     <th>Ticker</th>
+     <th>Price</th>
+     <th>Shares</th>
+     <th>Total</th>
+    <th colwidth="2">(negative shares = sell)</th>
+   </tr>
+   
+   <? foreach ($model['stocks'] as $stock): ?>
+   <tr>
+     <td><?= $stock->TICKER ?></td>
+     <td id="stock-price-<?= $stock->ID ?>"></td>
+    
+    <td id="stock-shares-<?= $stock->ID ?>">
+       <?= $stock->SHARES ?>
+     </td>
 
-	</tr>
-	
-	<!-- recommend this stock to your friends -->
+    <!-- because we cannot get the innerHTML of the shares element in FBJS -->
+    <? if ($stock->SHARES): ?>
+      <script> 
+          portfolioShares[<?= $stock->ID ?>] = <?= $stock->SHARES ?>; 
+      </script>
+    <? endif; ?>
+ 
+     <td id="stock-total-<?= $stock->ID ?>"></td>
+     <td class="row-controls">
+       <div>
+
+         <!-- buy/sell form for this stock -->
+         <form action="<?= $model['appUrl'] ?>/tradeStock">
+           <input type="submit" value="Buy/Sell" class="trade-button"/>
+           <input type="hidden" name="stockId" value="<?= $stock->ID ?>"/>
+           <input type="text" name="shares" class="shares"/>           
+           shares.
+         </form>
+
+         <!-- recommend this stock to your friends -->
          <a href="<?= $model['appUrl'] ?>/recommendStockToFriends?ticker=<?= $stock->TICKER ?>"
             class="recommend-control">
            Recommend to your friends!
