24f079ff6c06e5ce4dd31c53121e7eec2927be11
Astarcorp <corepbl@hotmail.com>
2013-11-22 13:30:18 +0800

    aaa

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -5,32 +5,23 @@ try {
 
 	$db = $m->tradingwithfriends;
 
-	echo "<h2>Collections</h2>";
-	echo "<ul>";
+	$log = $db->createCollection(
+    "logger",
+    array(
+        'capped' => true,
+        'size' => 10*1024,
+        'max' => 10
+    )
+);
 
-	// print out list of collections
-	$cursor = $db->listCollections();
-	$collection_name = "";
-	foreach( $cursor as $doc ) {
-		echo "<li>" .  $doc->getName() . "</li>";
-		$collection_name = $doc->getName();
-	}
-	echo "</ul>";
-
-	// print out last collection
-	if ( $collection_name != "" ) {
-		$collection = $db->selectCollection($collection_name);
-		echo "<h2>Documents in ${collection_name}</h2>";
-
-		// only print out the first 5 docs
-		$cursor = $collection->find();
-		$cursor->limit(5);
-		echo $cursor->count() . ' document(s) found. <br/>';
-		foreach( $cursor as $doc ) {
-		echo "<pre>";
-		var_dump($doc);
-		echo "</pre>";
+for ($i = 0; $i < 100; $i++) {
+    $log->insert(array("level" => WARN, "msg" => "sample log message #$i", "ts" => new MongoDate()));
 }
+
+$msgs = $log->find();
+
+foreach ($msgs as $msg) {
+    echo $msg['msg']."\n";
 }
 
 		// disconnect from server
