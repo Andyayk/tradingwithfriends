7ba9d7223f98bd199c1ff6de1f74d91321958708
Astarcorp <corepbl@hotmail.com>
2013-11-22 14:03:03 +0800

    suan le

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -1,29 +1,60 @@
+<!-- PHP Mongo Docs: http://php.net/manual/en/class.mongodb.php -->
+<html>
+<body>
+<h1>MongoHQ Test</h1>
 <?php
-	
-	$m = new MongoClient();
+try {
+	// connect to MongoHQ assuming your MONGOHQ_URL environment
+	// variable contains the connection string
+	$connection_url = getenv("MONGOHQ_URL");
 
-	$db = $m->tradingwithfriends;
+	// create the mongo connection object
+	$m = new Mongo($connection_url);
 
-	$log = $db->createCollection(
-    "logger",
-    array(
-        'capped' => true,
-        'size' => 10*1024,
-        'max' => 10
-    )
-);
+	// extract the DB name from the connection path
+	$url = parse_url($connection_url);
+	$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
 
-for ($i = 0; $i < 100; $i++) {
-    $log->insert(array("level" => WARN, "msg" => "sample log message #$i", "ts" => new MongoDate()));
+	// use the database we connected to
+	$db = $m->selectDB($db_name);
+
+	echo "<h2>Collections</h2>";
+	echo "<ul>";
+
+	// print out list of collections
+	$cursor = $db->listCollections();
+	$collection_name = "";
+	foreach( $cursor as $doc ) {
+		echo "<li>" .  $doc->getName() . "</li>";
+		$collection_name = $doc->getName();
+	}
+	echo "</ul>";
+
+	// print out last collection
+	if ( $collection_name != "" ) {
+		$collection = $db->selectCollection($collection_name);
+		echo "<h2>Documents in ${collection_name}</h2>";
+
+		// only print out the first 5 docs
+		$cursor = $collection->find();
+		$cursor->limit(5);
+		echo $cursor->count() . ' document(s) found. <br/>';
+		foreach( $cursor as $doc ) {
+		echo "<pre>";
+		var_dump($doc);
+		echo "</pre>";
 }
-
-$msgs = $log->find();
-
-foreach ($msgs as $msg) {
-    echo $msg['msg']."\n";
 }
 
 		// disconnect from server
 		$m->close();
-
+} catch ( MongoConnectionException $e ) {
+die('Error connecting to MongoDB server');
+} catch ( MongoException $e ) {
+die('Mongo Error: ' . $e->getMessage());
+} catch ( Exception $e ) {
+die('Error: ' . $e->getMessage());
+}
 ?>
+</body>
+</html>
