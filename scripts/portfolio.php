bf988466f257c25ecdb9732ed85492698ad1e7b1
Astarcorp <corepbl@hotmail.com>
2013-11-23 13:09:04 +0800

    asdada

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -1,37 +1,22 @@
 <?php 
-echo "Dont touch this until we have settled with putting data into DATABASE";
-
-/*session_start();
-	
-$user = '';
-
-$loggedIn = (!empty($_SESSION['user']));
-	
-if ($loggedIn) {
-	$user = $_SESSION['user'];
-} else {
-	header('Location: index.php');
-	exit;
-}
 
 require_once('config/database.php');
 	
 $mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); 
 	
-$stmt = $mysqli->prepare("SELECT * FROM `assignment_speeches`"); 
+$stmt = $mysqli->prepare("SELECT * FROM `portfolio`"); 
 
 $stmt->execute(); 
 
-$stmt->bind_result($id, $subject, $body, $tags, $image);
+$stmt->bind_result($name, $quantity, $price, $total);
 
-$speeches = array();
+$portfolioData = array();
 while ($stmt->fetch()) {
-	$speeches[$id] = array(
-		'id' => $id,
-		'subject' => $subject,
-		'body' => $body,
-		'tags' => $tags,
-		'image' => $image
+	$portfolioData[$id] = array(
+		'name' => $name,
+		'quantity' => $quantity,
+		'price' => $price,
+		'total' => $total
 	);
 }
 
@@ -41,8 +26,4 @@ $mysqli->close();
 
 $message = ''; 
 	
-if (!empty($_SESSION['message'])) {
-	$message = $_SESSION['message'];
-	unset($_SESSION['message']);
-}*/
 ?>
