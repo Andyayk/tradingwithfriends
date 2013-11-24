34cc05033c1357c2a2ac43d169ca7978113e1c30
Astarcorp <corepbl@hotmail.com>
2013-11-24 16:22:23 +0800

    dsa

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
 	  <div id="purchasingformButton">Purchasing Form</div>
 	  <div id="showForm">
 	  <p>
-	  	<div>Tip: To shortsell an equity, type in negative quantity</div>
+	  	<div>Tip: To sell an equity, type in negative quantity</div>
 		<b>Symbol:</b>
 		<select name="name" >
 			<option value="">Select Equity</option>
