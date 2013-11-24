9d11e60392ecfd94f0962df9ea776c481f3eb79c
Astarcorp <corepbl@hotmail.com>
2013-11-24 16:20:49 +0800

    ada

--- scripts/cash.php
+++ scripts/cash.php
@@ -4,15 +4,24 @@ require_once('config/database.php');
 
 $mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); 
 
-$stmt = $mysqli->prepare("SELECT cash FROM `portfolio` WHERE username = ?"); 
+$stmt = $mysqli->prepare("SELECT cash, id FROM `portfolio` WHERE username = ?"); 
 
 $stmt->bind_param("s", $username);
 
 $stmt->execute(); 
 
-$stmt->bind_result($cash);
+$stmt->bind_result($cash, $id);
 
-$stmt->fetch();
+$portfolioEquities = array();
+while ($stmt->fetch()) {
+	$portfolioEquities[$id] = array(
+		'cash' => $cash
+	);
+}
+
+foreach($portfolioEquities as $key => $portfolioEquity){
+	$cash = $portfolioEquity['cash'];
+}
 
 $isCashValid = !empty($cash);
 
