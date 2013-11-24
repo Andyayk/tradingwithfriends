4c911eb1a5599cc1e867f8917fb0f12361d3b27b
junwei <corepbl@hotmail.com>
2013-11-24 17:28:40 +0800

    test10

--- index.php
+++ index.php
@@ -154,7 +154,7 @@
       
       <div id="fb-root"></div>
       <script src="//connect.facebook.net/en_US/all.js"></script>
-      <p style="font-size: 80%;"> <p style="text-align: center;"><?php echo "Welcome " . $username;?></p></p><br/>
+      <p style="font-size: 80%;"><p style="text-align: center;"><?php echo "Welcome " . $username;?></p></p><br/>
       <span id="date_time"></span>
 	  <script type="text/javascript">window.onload = date_time('date_time');</script>
       
