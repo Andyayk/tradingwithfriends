c08e5d0cdacd8bca66ad9e8f5bbe3595c4c3b68d
Astarcorp <corepbl@hotmail.com>
2013-11-27 15:16:12 +0800

    fdgdf

---
+++ scripts/userupdatedatabase2.php
@@ -0,0 +1,17 @@
+<?php
+	
+	require_once('config/database.php');
+
+	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); 
+
+	$stmt = $mysqli->prepare("UPDATE `portfolio` SET `quantity` = ? WHERE `name` = ? AND `price` = ?"); 
+
+	$stmt->bind_param("sss", $newQuantity, $name, $price);
+
+	$successfullyUpdated = $stmt->execute(); 
+
+	$stmt->close();
+	
+	$mysqli->close();
+
+?>
