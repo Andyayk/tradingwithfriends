5119179be3d491a3d135c5b3fc88583b48f2053b
Astarcorp <corepbl@hotmail.com>
2013-11-27 16:02:40 +0800

    adasdas

--- scripts/quantity.php
+++ scripts/quantity.php
@@ -4,25 +4,23 @@
 
 	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
 
-	$stmt = $mysqli->prepare("SELECT quantity, id, price FROM `portfolio` WHERE `username` = ? AND `name` = ? AND `price` = ?"); //Select quantity, price and id from portfolio
+	$stmt = $mysqli->prepare("SELECT quantity, id FROM `portfolio` WHERE `username` = ? AND `name` = ? AND `price` = ?"); //Select quantity and id from portfolio
 
 	$stmt->bind_param("sss", $username, $name, $price);
 
 	$stmt->execute(); 
 
-	$stmt->bind_result($oldQuantity, $id, $oldPrice);
+	$stmt->bind_result($oldQuantity, $id);
 	
 	$checkPortfolio = array(); //Fetch and store in array
 	while ($stmt->fetch()) {
 		$checkPortfolio[$id] = array(
 			'quantity' => $oldQuantity,
-			'price' => $oldPrice
 		);
 	}
 
 	foreach($checkPortfolio as $key => $checkKey){ //Get lastest quantity
 		$oldQuantity = $checkKey['quantity'];
-		$oldPrice = $checkKey['price'];
 	}
 
 	$isQuantityValid = !empty($oldQuantity); //Quantity is not empty
@@ -33,7 +31,6 @@
 
 	if ($isQuantityValid){ //Make sure quantity is valid
 		$oldQuantity = $oldQuantity;
-		$oldPrice = $oldPrice;
 	} else { //Else original quantity the user posted
 		$quantity = $quantity;
 		$oldQuantity = 0;
--- scripts/quantity2.php
+++ scripts/quantity2.php
@@ -4,23 +4,25 @@
 
 	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
 
-	$stmt = $mysqli->prepare("SELECT quantity, id FROM `portfolio` WHERE `username` = ? AND `name` = ? "); //Select quantity and id from portfolio
+	$stmt = $mysqli->prepare("SELECT quantity, id, price FROM `portfolio` WHERE `username` = ? AND `name` = ? "); //Select quantity, price and id from portfolio
 
 	$stmt->bind_param("ss", $username, $name);
 
 	$stmt->execute(); 
 
-	$stmt->bind_result($oldQuantity, $id);
+	$stmt->bind_result($oldQuantity, $id, $oldPrice);
 	
 	$checkPortfolio = array(); //Fetch and store in array
 	while ($stmt->fetch()) {
 		$checkPortfolio[$id] = array(
 			'quantity' => $oldQuantity,
+			'price' => $oldPrice
 		);
 	}
 
 	foreach($checkPortfolio as $key => $checkKey){ //Get lastest quantity
 		$oldQuantity = $checkKey['quantity'];
+		$oldPrice = $checkKey['price'];
 	}
 
 	$isQuantityValid = !empty($oldQuantity); //Quantity is not empty
@@ -31,6 +33,7 @@
 
 	if ($isQuantityValid){ //Make sure quantity is valid
 		$oldQuantity = $oldQuantity;
+		$oldPrice = $oldPrice;
 	} else { //Else original quantity the user posted
 		$quantity = $quantity;
 		$oldQuantity = 0;
