d242a443615dd2c90f74ecab32b02b773baa05c2
Astarcorp <corepbl@hotmail.com>
2013-11-30 12:40:52 +0800

    asfas

--- scripts/order.php
+++ scripts/order.php
@@ -1,6 +1,25 @@
 <?php 
 	
- 	require_once ('config/database.php'); //Login to database
+ 	$url = $_SERVER['HTTP_HOST'];
+
+	$thisIsLocalHostUrl = (strpos($url, 'localhost') !== false);
+	$thisIsCloudControlUrl = (strpos($url, 'cloudcontrolled') !== false);
+
+	//Credentials for localhost database
+	if ($thisIsLocalHostUrl) {
+		$database_name = 'tradingwithfriends';
+		$database_username = 'root';
+		$database_password = '';
+		$database_hostname = 'localhost';
+	}
+	
+	//Credentials for cloudcontrol database
+	if ($thisIsCloudControlUrl) {
+		$database_name = 'depqdrc7d65';
+		$database_username = 'depqdrc7d65';
+		$database_password = 'p6A2wObwmahl';
+		$database_hostname = 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com';
+	}
 
 	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
 
