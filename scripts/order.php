5915a120f44c76a8f943aed6c350857604c15ff9
Astarcorp <corepbl@hotmail.com>
2013-11-30 13:11:28 +0800

    sda

--- scripts/order.php
+++ scripts/order.php
@@ -1,6 +1,8 @@
 <?php 
 	
-	require_once('config/database.php'); //Login to database
+	$username = 'Andy Ang Yong Kiat';	
+
+	require('config/database.php'); //Login to database
 
 	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
 
