b08b62f4696b1e25472e4e5198912e16bf10ef1c
Astarcorp <corepbl@hotmail.com>
2013-11-30 14:12:17 +0800

    6545

--- scripts/order.php
+++ scripts/order.php
@@ -23,20 +23,21 @@
 
 	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
 
-	$stmt = $mysqli->prepare("SELECT name, quantity, id FROM `portfolio` WHERE `username` = ?"); //Select username, name, quantity, id, order and orderprice from portfolio
+	$stmt = $mysqli->prepare("SELECT name, quantity, id, order FROM `portfolio` WHERE `username` = ?"); //Select username, name, quantity, id, order and orderprice from portfolio
 
 	$stmt->bind_param("s", $username);
 
 	$stmt->execute(); 
 
-	$stmt->bind_result($name, $quantity, $oldId);
+	$stmt->bind_result($name, $quantity, $oldId, $order, $orderPrice);
 
 	$orderArray = array(); //Fetch and store in array
 	while ($stmt->fetch()) {
 		$orderArray[$id] = array(
 			'name' => $name,
 			'quantity' => $quantity,
-			'id' => $oldId
+			'id' => $oldId,
+			'order' => $order,
 		);
 	}
 	
@@ -45,6 +46,13 @@
 		$name = $orderKey['name'];
 		$quantity = $orderKey['quantity'];
 		$oldId = $orderKey['id'];
+		$order = $orderKey['order'];
+		$orderPrice = $orderKey['orderprice'];
+
+		if ($order = "Stop Loss Order" ){
+		echo "hello";
+
+		}
 	}
 
 	$stmt->close();
