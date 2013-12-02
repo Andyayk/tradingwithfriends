9fe42695d38549258ef1909b98a17850f84cc487
junwei <corepbl@hotmail.com>
2013-12-02 10:21:31 +0800

    5

--- index.php
+++ index.php
@@ -344,7 +344,7 @@
 	  
 	  <div id="historyButton">History of Transactions</div>
 	  <div id="showHistory">
-	  <div style="border:1px solid black;width:100px;height:100px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+	  <div style="border:1px solid black;width:200px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
 	  <p style="height:150%;">
 	  <?php require 'scripts/history.php';?>
 	  </div>
