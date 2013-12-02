a2c0f96ede4758d63310d42d8ae28ca28f736c75
junwei <corepbl@hotmail.com>
2013-12-02 09:46:40 +0800

    1

--- scripts/date_time.js
+++ scripts/date_time.js
@@ -30,6 +30,6 @@
     
 	result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;    
 	document.getElementById(id).innerHTML = result;  
-	setTimeout('date_time("'+id+'");','500');
+	setTimeout('date_time("'+id+'");','1000');
     return true;
   }
