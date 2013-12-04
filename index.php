61f6aeba1de99575a80110987804dde87a2bc25c
Astarcorp <corepbl@hotmail.com>
2013-12-04 14:06:21 +0800

    sadaasdsa

--- index.php
+++ index.php
@@ -256,139 +256,139 @@
   <head>
     <title>Trading with Friends</title>
     
-   	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      <meta property="og:image" content="https://github.com/Astarcorp/tradingwithfriends/blob/master/images/logo_large.jpg"/>
+   	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta property="og:image" content="https://github.com/Astarcorp/tradingwithfriends/blob/master/images/logo_large.jpg"/>
 
-      <link href="scripts/style.css" rel="stylesheet" type="text/css">
+    <link href="scripts/style.css" rel="stylesheet" type="text/css">
     
-      <script type="text/javascript" src="scripts/date_time.js"></script>
-      <script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="scripts/date_time.js"></script>
+    <script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
       
-      <script> 
-      $(document).ready(function(){
-        var auto_refresh = setInterval(
-		  function (){
-		  $("#showEquity").load("scripts/equity.php").fadeIn("slow");
-	    }, 30000); //refresh every 30000 milliseconds
-      });
-      </script>
+    <script> 
+    $(document).ready(function(){
+    	var auto_refresh = setInterval(
+			function (){
+			$("#showEquity").load("scripts/equity.php").fadeIn("slow");
+		}, 30000); //refresh every 30000 milliseconds
+    });
+    </script>
       
-      <script> 
-      $(document).ready(function(){
-        var auto_refresh = setInterval(
-		  function (){
-		  $("#orderprocessing").load("scripts/order.php");
-	    }, 30000); //refresh every 30000 milliseconds
-      });
-      </script>
+    <script> 
+    $(document).ready(function(){
+    	var auto_refresh = setInterval(
+			function (){
+			$("#orderprocessing").load("scripts/order.php");
+		}, 30000); //refresh every 30000 milliseconds
+	});
+	</script>
   </head>
   <body>
-      <div id="fb-root"></div>
-      <script src="//connect.facebook.net/en_US/all.js"></script>
+	<div id="fb-root"></div>
+    <script src="//connect.facebook.net/en_US/all.js"></script>
       
-      <div id="topbar">
-      <img src="images/logo.jpg"/>
-      </div>
-      <span id="date_time"></span>
-      <script type="text/javascript">window.onload = date_time('date_time');</script>
+    <div id="topbar">
+    <img src="images/logo.jpg"/>
+    </div>
+    <span id="date_time"></span>
+    <script type="text/javascript">window.onload = date_time('date_time');</script>
       
-      <script>
- 	  Date.short_months= ['Jan', 'Feb', 'Mar', 'Apr', 'May',
-      'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      Date.tzones={
-      N:['Newfoundland', -210],
-      A:['Atlantic', -240],
-      E:['Eastern', -300],
-      C:['Central', -360],
-      M:['Mountain', -420],
-      P:['Pacific', -480],
-      AK:['Alaska', -540],
-      HA_:['Hawaii-Aleutian (Aleutian)', -600],
-      HA:['Hawaii-Aleutian (Hawaii)', -600, -600]
-      };
-      Date.dstOff= function(d, tz){
-      var off= tz[1], doff= tz[2],
-      countstart, countend, dstart, dend,
-      y= d.getUTCFullYear();
-      if(y>2006 && off!== doff){
-      countstart= 8, countend= 1,
-      dstart= new Date(Date.UTC(y, 2, 8, 2)),
-      dend= new Date(Date.UTC(y, 10, 1, 2));
-      while(dstart.getUTCDay()!== 0){
-      dstart.setUTCDate(++countstart);
-      }
-      while(dend.getUTCDay()!== 0){
-      dend.setUTCDate(++countend);
-      }
-      dstart.setUTCMinutes(off);
-      dend.setUTCMinutes(off);
-      if(dstart<= d && dend>= d) off= doff;
-      }
-      return off;
-      }
-      Date.toTZString= function(d, tzp){
-      d= d? new Date(d):new Date();
-      tzp= tzp || 'G';
-      var h, m, s, pm= 'pm', off, dst, str,
-      label= tzp+'ST',
-      tz= Date.tzones[tzp.toUpperCase()];
-      if(!tz) tz= ['Greenwich', 0, 0];
-      off= tz[1];
-      if(off){
-      if(tz[2]== undefined) tz[2]= tz[1]+60;
-      dst= Date.dstOff(d, tz);
-      if(dst!== off) label= tzp+'DT';
-      d.setUTCMinutes(d.getUTCMinutes()+dst);
-      }
-      else label= 'GMT';
-      h= d.getUTCHours();
-      m= d.getUTCMinutes();
-      if(h>12) h-= 12;
-      else if(h!== 12) pm= 'am';
-      if(h== 0) h= 12;
-      if(m<10) m= '0'+m;
-      var str= Date.short_months[d.getUTCMonth()]+' '+d.getUTCDate()+', ';
-      return str+ h+':'+m+' '+pm+' '+label.replace('_', '').toUpperCase();
-      }
-      window.onload=function(){
-      var who=document.getElementById('CentralTimer');
-      who.firstChild.data= Date.toTZString('', 'C');
-      Date.ctclock= setInterval(function(){
-      var v=who.firstChild.data,
-      t=Date.toTZString('', 'C');
-      if(v!=t) who.firstChild.data=t;
-      },1000);
-      who.ondblclick=function(){
-      clearInterval(Date.ctclock);
-      who.firstChild.data+=' (Clock Stopped)';
-      }
-      }
-	  </script>
+    <script>
+ 	Date.short_months= ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+    'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    Date.tzones={
+    N:['Newfoundland', -210],
+    A:['Atlantic', -240],
+    E:['Eastern', -300],
+    C:['Central', -360],
+    M:['Mountain', -420],
+    P:['Pacific', -480],
+    AK:['Alaska', -540],
+    HA_:['Hawaii-Aleutian (Aleutian)', -600],
+    HA:['Hawaii-Aleutian (Hawaii)', -600, -600]
+    };
+    Date.dstOff= function(d, tz){
+    var off= tz[1], doff= tz[2],
+    countstart, countend, dstart, dend,
+    y= d.getUTCFullYear();
+    if(y>2006 && off!== doff){
+    countstart= 8, countend= 1,
+    dstart= new Date(Date.UTC(y, 2, 8, 2)),
+    dend= new Date(Date.UTC(y, 10, 1, 2));
+    while(dstart.getUTCDay()!== 0){
+    dstart.setUTCDate(++countstart);
+    }
+    while(dend.getUTCDay()!== 0){
+    dend.setUTCDate(++countend);
+    }
+    dstart.setUTCMinutes(off);
+    dend.setUTCMinutes(off);
+    if(dstart<= d && dend>= d) off= doff;
+    }
+    return off;
+    }
+    Date.toTZString= function(d, tzp){
+    d= d? new Date(d):new Date();
+    tzp= tzp || 'G';
+    var h, m, s, pm= 'pm', off, dst, str,
+    label= tzp+'ST',
+    tz= Date.tzones[tzp.toUpperCase()];
+    if(!tz) tz= ['Greenwich', 0, 0];
+    off= tz[1];
+    if(off){
+    if(tz[2]== undefined) tz[2]= tz[1]+60;
+    dst= Date.dstOff(d, tz);
+    if(dst!== off) label= tzp+'DT';
+    d.setUTCMinutes(d.getUTCMinutes()+dst);
+    }
+    else label= 'GMT';
+    h= d.getUTCHours();
+    m= d.getUTCMinutes();
+    if(h>12) h-= 12;
+    else if(h!== 12) pm= 'am';
+    if(h== 0) h= 12;
+    if(m<10) m= '0'+m;
+    var str= Date.short_months[d.getUTCMonth()]+' '+d.getUTCDate()+', ';
+    return str+ h+':'+m+' '+pm+' '+label.replace('_', '').toUpperCase();
+    }
+    window.onload=function(){
+    var who=document.getElementById('CentralTimer');
+    who.firstChild.data= Date.toTZString('', 'C');
+    Date.ctclock= setInterval(function(){
+    var v=who.firstChild.data,
+    t=Date.toTZString('', 'C');
+    if(v!=t) who.firstChild.data=t;
+    },1000);
+    who.ondblclick=function(){
+    clearInterval(Date.ctclock);
+    who.firstChild.data+=' (Clock Stopped)';
+    }
+    }
+	</script>
 	  
-      <p style="text-align: center;"><b><?php echo "Welcome " . $username . " to Trading with Friends!";?></b></p>
-      <p style="text-align: center;"><b><?php echo "Currently, you have $" . $cash . " to spend on trading.";?></b></p>
+    <p style="text-align: center;"><b><?php echo "Welcome " . $username . " to Trading with Friends!";?></b></p>
+    <p style="text-align: center;"><b><?php echo "Currently, you have $" . $cash . " to spend on trading.";?></b></p>
       
-      <script src="scripts/userinterface.js"></script>
+    <script src="scripts/userinterface.js"></script>
       
-      <?php echo $message; ?>
+    <?php echo $message; ?>
       
-      <div id="equityButton">Equities List</div>
-	  <div id="showEquity">
-	  <div style="border:1.5px solid black;width:900px;height:400px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
-	  <p style="height:100%;">
-      <?php require 'scripts/equity.php';?>
-	  </div>
-	  </p>
-	  </div>
+    <div id="equityButton">Equities List</div>
+	<div id="showEquity">
+		<div style="border:1.5px solid black;width:900px;height:400px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+	 		<p style="height:100%;">
+      			<?php require 'scripts/equity.php';?>
+	  		</p>
+ 		</div>
+	</div>
 	  
-	  <div id="purchasingformButton">Purchasing Form</div>
-	  <div id="showForm">
-	  <form name="buyForm" method="post">
-	  <p>
-	  	<div><i>Tip: Check everything before submitting to prevent mistakes</i></div></br></br>
+	<div id="purchasingformButton">Purchasing Form</div>
+	<div id="showForm">
+	<form name="buyForm" method="post">
+	<p>
+		<div><i>Tip: Check everything before submitting to prevent mistakes</i></div></br>
 		<b>Equity:</b>
 		<select id=name name="name" >
 			<option value="">Select Equity</option>
@@ -410,68 +410,68 @@
 		<font color="red"><?php echo $orderError; ?></font>
 		
 		<b>at:</b> <input type="text" name="orderPrice" value="<?php if(!empty($_POST['orderPrice']))echo $_POST['orderPrice']; ?>" /> <font color="red"><?php echo $orderpriceError; ?></font>
-	  </p>
+	</p>
 	  
-	  <p>
-	  <input type="submit" name="buySubmit" value="Submit" />
-	  </p>
-	  </div>
-	  </form>
+	<p>
+		<input type="submit" name="buySubmit" value="Submit" />
+	</p>
+	</form>
+	</div>
 	  
-	  <div id="sellingformButton">Selling Form</div>
-	  <div id="showsellForm">
-	  <form name="sellForm" method="post">
-	  <p>
+	<div id="sellingformButton">Selling Form</div>
+	<div id="showsellForm">
+	<form name="sellForm" method="post">
+	<p>
 	  	<div><i>Tip: Check everything before submitting to prevent mistakes</i></div></br></br>
 		<b>ID:</b> <input type="text" name="id" value="<?php if(!empty($_POST['id']))echo $_POST['id']; ?>" /> <font color="red"><?php echo $idError; ?></font><br/>
 		<b>Quantity:</b> <input type="text" name="sellQuantity" value="<?php if(!empty($_POST['sellQuantity']))echo $_POST['sellQuantity']; ?>" /> <font color="red"><?php echo $sellquantityError; ?></font>
-	  </p>
+	</p>
 	  
-	  <p>
+	<p>
 	  	<input type="submit" name="sellSubmit" value="Submit" />
-	  </p>
-	  </div>
-	  </form>
+	</p>
+	</form>
+	</div>
 	  
-	  <div id="portfolioButton">My Portfolio</div>
-	  <div id="showPortfolio">
-	  	<div style="border:1px solid black;width:500px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+	<div id="portfolioButton">My Portfolio</div>
+	<div id="showPortfolio">
+		<div style="border:1px solid black;width:500px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
 	  		<p style="height:150%;">
 	  			<?php require 'scripts/portfolio.php';?>
 	  		</p>
-	  	</div>
-	  </div>
+		</div>
+	</div>
 	  
-	  <div id="historyButton">History of Transactions</div>
-	  <div id="showHistory">
-	  <div style="border:1px solid black;width:500px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
-	  <p style="height:150%;">
-	  <?php require 'scripts/history.php';?>
-	  </p>
-	  </div>
-	  </div>
+	<div id="historyButton">History of Transactions</div>
+	<div id="showHistory">
+	  	<div style="border:1px solid black;width:500px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+	  		<p style="height:150%;">
+	  			<?php require 'scripts/history.php';?>
+	 		 </p>
+	  	</div>
+	</div>
   
-	  <div id="recommendButton">Recommend this app to your friends!!</div>
-	  <div id="postButton">Post on Facebook!!</div>    
+	<div id="recommendButton">Recommend this app to your friends!!</div>
+	<div id="postButton">Post on Facebook!!</div>    
       
-      <script>
-      var appId = '<?php echo $facebook->getAppID() ?>';
+    <script>
+    var appId = '<?php echo $facebook->getAppID() ?>';
 
-      //Initialize the JS SDK
-      FB.init({
-        appId: appId,
-        frictionlessRequests: true,
-        cookie: true,
-      });
+    //Initialize the JS SDK
+    FB.init({
+      appId: appId,
+      frictionlessRequests: true,
+      cookie: true,
+    });
 
-      FB.getLoginStatus(function(response) {
-        uid = response.authResponse.userID ? response.authResponse.userID : null;
-      });
-      </script>
+    FB.getLoginStatus(function(response) {
+      uid = response.authResponse.userID ? response.authResponse.userID : null;
+    });
+    </script>
       
-      <b>Any Questions to Ask?? </b><a href="http://astartalk.forumotion.com/" target="_blank">Discuss it on our Forum!!</a>
-      <br /><b>Interested to Find Out More About Equities?? </b><a href="http://astartalk.forumotion.com/">Visit our Website now!!</a>  
+    <b>Any Questions to Ask?? </b><a href="http://astartalk.forumotion.com/" target="_blank">Discuss it on our Forum!!</a>
+    <br/><b>Interested to Find Out More About Equities?? </b><a href="http://astartalk.forumotion.com/">Visit our Website now!!</a>  
       
-      <div id="orderprocessing"><?php require 'scripts/order.php';?></div>
-</body>
+    <div id="orderprocessing"><?php require 'scripts/order.php';?></div>
+  </body>
 </html>
