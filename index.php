ce59ad74623b45a5ebe367f9bd0f059d89dd1cd3
junwei <corepbl@hotmail.com>
2013-12-05 16:49:28 +0800

    14

--- index.php
+++ index.php
@@ -325,9 +325,9 @@
     </div>
     
     <span id="date_time"></span>
-    <script type="text/javascript">window.onload = date_time('date_time');</script>
+    <script type="text/javascript">window.onload = date_time('date_time');</script></br>
     <?php $date = new DateTime('now', new DateTimeZone('America/New_York'));
-     echo $date->format('d-m-Y H:i:s');?>
+     echo "US timing " .$date->format('d-m-Y H:i:s');?>
 		  
     <p style="text-align: center;"><b><?php echo "Welcome " . $username . " to Trading with Friends!";?></b></p>
     <p style="text-align: center;"><b><?php echo "Currently, you have $" . $cash . " to spend on trading.";?></b></p>
