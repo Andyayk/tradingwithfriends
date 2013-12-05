38b261742258ec769282ce03998b3ef3eda7f4b3
Astarcorp <corepbl@hotmail.com>
2013-12-05 16:24:47 +0800

    sdfsd

--- scripts/portfolio.php
+++ scripts/portfolio.php
@@ -45,9 +45,9 @@
 	<tr>
 		<td>
 			<b>ID:</b> <?php echo $id; ?>
-			You owned <?php echo $quantity; ?> <?php echo $name; ?>
+			<?php echo $quantity; ?> <?php echo $name; ?>
 			equities at <?php echo $price; ?>
-			each, with a <?php echo $order; ?>
+			each, <?php echo $order; ?>
 			order at <?php echo $orderPrice; ?>.
 		</td>
   	</tr>
