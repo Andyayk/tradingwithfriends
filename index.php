a4ccaa4a7fac8cce0071068a3f4b99b0229c4cad
Astarcorp <corepbl@hotmail.com>
2013-12-06 11:49:38 +0800

    the one that got away

--- index.php
+++ index.php
@@ -382,7 +382,7 @@
 	</form>
 	</div>
 	  
-	<div id="showsellForm" style="display:inline-block;" style="float:right;">
+	<div id="showsellForm" style="float:right;">
 	<form name="sellForm" method="post">
 	<p>
 	  	<i>Tip: Check everything before submitting to prevent mistakes</i></br>
