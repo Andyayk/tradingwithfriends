66e1e5ce031ce85366f62b2876d6e4e7b6e95885
Astarcorp <corepbl@hotmail.com>
2013-12-07 16:34:12 +0800

    465

--- index.php
+++ index.php
@@ -407,7 +407,7 @@
 	<div id="portfolioButton" style="display:inline-block;">My Portfolio </div> 
 	<div id="historyButton" style="display:inline-block;">History of Transactions</div>
 	
-	<div id="showPortfolio" style="display:none;">
+	<div id="showPortfolio" style="display:inline-block; display:none;">
 		<div style="border:1px solid black;width:600px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
 	  		<p style="height:100%;">
 	  			<?php require 'scripts/portfolio.php';?>
@@ -415,7 +415,7 @@
 		</div>
 	</div>
 	 
-	<div id="showHistory" style="display:inline-block;">
+	<div id="showHistory" style="display:none;">
 	  	<div style="border:1px solid black;width:600px;height:200px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
 	  		<p style="height:100%;">
 	  			<?php require 'scripts/history.php';?>
