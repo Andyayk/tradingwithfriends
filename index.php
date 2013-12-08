0446a59e54960f2947840b8b78ed2de198b6826b
Astarcorp <corepbl@hotmail.com>
2013-12-08 16:35:30 +0800

    asdas

--- index.php
+++ index.php
@@ -301,6 +301,9 @@
 	
   	}
   } else { //Welcome user
+  	$message = '';
+	
+	$message = $message . "\t\t" . '<font size="4" color="orange">Welcome to Trading with Friends!!</font><br />' . "\n";
   	echo "<script language=javascript>alert('Welcome to Trading with Friends!!')</script>";
   } 
 ?>
