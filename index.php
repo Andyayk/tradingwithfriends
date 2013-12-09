b0e75dcbaf633307a71a0a02722ae682ce2ecbf1
Astarcorp <corepbl@hotmail.com>
2013-12-09 10:04:55 +0800

    asdas

--- index.php
+++ index.php
@@ -353,8 +353,7 @@
     <div id="welcome">
     <p style="text-align: left;"><b>Local Time: <span id="date_time"></span></b>
     <script type="text/javascript">window.onload = date_time('date_time');</script></br>
-    
-    <b><?php $date = new DateTime('now', new DateTimeZone('America/New_York')); echo "US Time: " .$date->format('l, F j Y G:i:s');?></b></p>
+    </p>
     
     <?php echo "<img src=\"" . $profile_pic . "\" />";?></br>
     <p style="text-align: center;"><b><?php echo "Hello " . $username . "! You have $" . $cash . " on hand now, let's start trading!!";?></b></p>
