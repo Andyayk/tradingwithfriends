7c949d51c267b268fe61d36806480f30bef15d03
Astarcorp <corepbl@hotmail.com>
2013-12-09 09:16:17 +0800

    asda

--- index.php
+++ index.php
@@ -347,7 +347,7 @@
     <script src="//connect.facebook.net/en_US/all.js"></script>
     
     <div id="topbar" style="text-align: center;">
-    <br /><img src="images/logo.png"/>
+    <br /><img src="images/logo.jpg"/>
     </div>
     
     <div id="welcome">
