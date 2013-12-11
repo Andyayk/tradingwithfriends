a9c277af3702b1f9bf79c4d2846fa4bd8e51e1d7
junwei <corepbl@hotmail.com>
2013-12-11 23:36:03 +0800

    come on

--- index.php
+++ index.php
@@ -366,12 +366,8 @@
 		<table border=1 cellspacing=1 cellpadding=2>
 	 		<tr class=head><th>Name</th><th>Symbol</th><th>Last Trade</th><th>Open</th><th>Close</th><th>Change</th><th>Bid</th><th>Ask</th><th>Volume</th><th>Day's Range</th><th>52wk Range</th><th>Earnings per Share</th><th>P/B Ratio</th><th>P/E Ratio</th><th>Last Trade Date</th><th>Last Trade Time</th><th>Chart</th></tr>
       	</table>
-			<div id="scrollEquity" style="border:1px solid black;width:1200px;height:250px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
-	 			<p style="height:100%;">
 				<?php require 'scripts/equity.php';?>
-	  		</p>
  		</div>
-	</div>
 	
 	<div id="orderprocessing"><?php require 'scripts/order.php';?></div>
 	
@@ -432,13 +428,19 @@
 	<div id="historyButton" style="display:inline-block;">History of Transactions</div>
 	
 	<div id="showPortfolio" style="display:inline-block;">
+		<div style=border:1px solid black;width:600px;height:100px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+		<p style="height:85%;"> 			
 	  			<?php require 'scripts/portfolio.php';?>
-	  		</p>
+	  			</p>
+	  			</div> 
 	</div>
 	 
 	<div id="showHistory" style="display:inline-block;">
-	  			<?php require 'scripts/history.php';?>
+	  			<div style=border:1px solid black;width:600px;height:100px;overflow:scroll;overflow-y:scroll;overflow-x:hidden;">
+				<p style="height:85%;"> 
+					<?php require 'scripts/history.php';?>	
 	 		 </p>
+	 		 </div>
 	</div>
     </div>
   
