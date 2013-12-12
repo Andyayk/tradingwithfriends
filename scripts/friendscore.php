e8170a9e358732f7f631173fab7ad692dfa87a48
Astarcorp <corepbl@hotmail.com>
2013-12-12 10:39:32 +0800

    you made it no refreshing

---
+++ scripts/friendscore.php
@@ -0,0 +1,38 @@
+<?php 
+
+	require_once('config/database.php'); //Login to database
+
+	$mysqli = new mysqli($database_hostname, $database_username, $database_password, $database_name) or exit("Error connecting to database"); //Connect
+
+	$stmt = $mysqli->prepare("SELECT cash, id FROM `history` WHERE `username` = ?");  //Select cash and id from history
+
+	$stmt->bind_param("s", $username);
+
+	$stmt->execute(); 
+
+	$stmt->bind_result($cash, $id);
+
+	$cashArray = array(); //Fetch and store in array
+	while ($stmt->fetch()) {
+		$cashArray[$id] = array(
+			'cash' => $cash
+		);
+	}
+
+	foreach($cashArray as $key => $cashKey){ //Get latest cash
+		$cash = $cashKey['cash'];
+	}
+
+	$isCashValid = !empty($cash); //Cash is not empty
+
+	$stmt->close();
+
+	$mysqli->close();
+
+	if ($isCashValid){ //Make sure cash is valid
+		$cash = $cash;
+	} else { //Else cash = 10000
+		$cash = 10000;
+	}
+
+?>
