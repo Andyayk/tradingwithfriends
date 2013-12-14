8a905b9ba40bc267ff3ba7e14d3edf8ce8817edc
Astarcorp <corepbl@hotmail.com>
2013-12-14 14:49:51 +0800

    dsfsddsf

--- index.php
+++ index.php
@@ -462,7 +462,7 @@
     
     <div id="showScore" style="display:inline-block;">
     <div style="width:570px;height:150px;line-height:1.5em;overflow:auto;padding:3px;">
-		<?php require 'friendscores.php'; ?>
+		<?php require 'scripts/friendscores.php'; ?>
     </div>
     </div>
     </div>
