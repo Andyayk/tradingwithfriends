152b3e7453b0afee58855c4d97b35054b888c5f9
junwei <corepbl@hotmail.com>
2013-12-18 14:00:09 +0800

    why

--- index.php
+++ index.php
@@ -404,13 +404,14 @@
 	</p>
     
     <div id="fb-root"></div>
+    <p style="text-align: left;">
 	<script>(function(d, s, id) {
   	var js, fjs = d.getElementsByTagName(s)[0];
   	if (d.getElementById(id)) return;
   	js = d.createElement(s); js.id = id;
   	js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
   	fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	}(document, 'script', 'facebook-jssdk'));</script></p>
 	
 	<p style="text-align: left;"><div class="fb-like" data-href="https://www.facebook.com/tradingwithfriends",'_blank" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
     </p>
